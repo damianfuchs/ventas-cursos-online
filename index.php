34251aeaf5b9fb0b53395b6c7f58d3ff15162188
Pablo <pablogabarro@gmail.com>
2023-10-19 22:12:46 -0300

    Sesiones corregidas

--- index.php
+++ index.php
@@ -7,11 +7,11 @@
 
 <?php
 if (isset($_SESSION['not_logged'])) {
-    $no_loggeado = $_SESSION['not_logged'];
+    $not_logged = $_SESSION['not_logged'];
     unset($_SESSION['not_logged']);
     echo '<div class="alert alert-warning text-center"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">
     <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
-  </svg>'.$no_loggeado.'</div>';
+  </svg>'.$not_logged.'</div>';
 } elseif (isset($_SESSION['mensaje_error'])) {
     $mensaje_error = $_SESSION['mensaje_error'];
     unset($_SESSION['mensaje_error']);
@@ -19,10 +19,11 @@ if (isset($_SESSION['not_logged'])) {
     <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
   </svg>'.$mensaje_error.'</div>';
 } elseif (isset($_SESSION['logged_out'])){
+  $logged_out = $_SESSION['logged_out'];
   unset($_SESSION['logged_out']);
   echo '<div class="alert alert-warning text-center"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">
     <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
-  </svg>'.$no_loggeado.'</div>';
+  </svg>'.$logged_out.'</div>';
 }
 ?>
 
--- logout.php
+++ logout.php
@@ -3,7 +3,7 @@ session_start();
 
 if (isset($_SESSION['mensaje_exito'])){
     unset($_SESSION['mensaje_exito']);
-    $_SESSION['not_logged'] = "Se ha deslogueado con éxito.";
+    $_SESSION['logged_out'] = "Se ha deslogueado con éxito.";
 }
 
 header('Location: index.php');
