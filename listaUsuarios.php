e1136ec39a8d96855720a07b8c0eb64745544b3d
damianfuchs <damianfuchs_1999@hotmail.com>
2023-11-09 00:16:23 -0300

    Agregado de: listaCursos.php listaUsuaios.php

---
+++ listaUsuarios.php
@@ -0,0 +1,16 @@
+<?php session_start(); ?>
+
+<?php include('includes/header.php'); ?>
+
+
+<link rel="stylesheet" href="estilos/index.css">
+<link rel="stylesheet" href="estilos/register.css">
+<link rel="stylesheet" href="estilos/addCourse.css">
+
+<link rel="stylesheet" href="estilos/index.css">
+<link rel="stylesheet" href="estilos/register.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+
+<?php include('includes/footer.php'); ?>
+<?php
