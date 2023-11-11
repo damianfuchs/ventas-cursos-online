862629baa35642901ad22735df94d76250b31334
Pablo <pablogabarro@gmail.com>
2023-11-10 21:50:20 -0300

    Correccion

--- cursos.php
+++ cursos.php
@@ -10,25 +10,21 @@ require_once 'includes/mysql/mysqlGetCursos.php';
 require_once 'includes/alert.php';
 // Llama a la función para obtener los datos
 $data = consultarCursos();
-/*if (isset($_SESSION['not_logged'])) {
-    $not_logged = $_SESSION['not_logged'];
-    unset($_SESSION['not_logged']);
-    echo '<div class="alert alert-warning text-center"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">
-    <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
-  </svg>'.$not_logged.'</div>';
-} elseif (isset($_SESSION['mensaje_error'])) {
-    $mensaje_error = $_SESSION['mensaje_error'];
-    unset($_SESSION['mensaje_error']);
-    echo '<div class="alert alert-danger text-center"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">
-    <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
-  </svg>'.$mensaje_error.'</div>';
-} elseif (isset($_SESSION['logged_out'])){
-  $logged_out = $_SESSION['logged_out'];
-  unset($_SESSION['logged_out']);
-  echo '<div class="alert alert-warning text-center"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">
-    <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
-  </svg>'.$logged_out.'</div>';
-}*/
+if (isset($_SESSION['unexpected_error'])){
+    $unexpected_error_title = $_SESSION['unexpected_error_title'];
+    $unexpected_error = $_SESSION['unexpected_error'];
+    unset($_SESSION['unexpected_error_title']);
+    unset($_SESSION['unexpected_error']);
+    $alert = new alerta($unexpected_error_title, $unexpected_error);
+    $alert->informar_error();
+} elseif (isset($_SESSION['deleted_success'])){
+    $deleted_success_title = $_SESSION['deleted_success_title'];
+    $deleted_success = $_SESSION['deleted_success'];
+    unset($_SESSION['deleted_success_title']);
+    unset($_SESSION['deleted_success']);
+    $alert = new alerta($deleted_success_title, $deleted_success);
+    $alert->informar_error();
+}
 ?>
 <body class="d-flex flex-column min-vh-100">
 <div class="container mt-5">
